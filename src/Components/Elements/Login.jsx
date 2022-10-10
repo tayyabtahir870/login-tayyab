6424c5c3e18255cc0b1dd6dcfc00766679c06be6
tayyabtahir870@gmail.com <tayyabtahir870@gmail.com>
2022-10-10 11:20:42 +0500

    login

--- src/Components/Elements/Login.jsx
+++ src/Components/Elements/Login.jsx
@@ -1,43 +1,63 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-scroll";
-import {BsFacebook} from 'react-icons/bs'
-import {AiFillTwitterCircle} from 'react-icons/ai'
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const formSubmit=(e)=>{
+      e.preventDefault()
+      console.log("email",email)
+      console.log("password",password)
+      // setEmail('');
+      // setPassword('');
+  }
   return (
     <div className="login">
       <div className="container p-5  ">
         <div className=" card text-center  py-5  ">
-          <h1>Login </h1> <br />
-          <h5>Lets go gets started</h5>
-          <br />
-          <h5>Username</h5>
-          <input
-            className="btn1"
-            type="text"
-            placeholder="Enter Your Username"
-          />
-          <br />
-          <br />
-          <h5>Password</h5>
-          <input className="btn1" type="password" placeholder="Enter Your Password" />
-          <br />
-          <br />
-          <button  type="button" class="btn btn-dark">
-            Submit
-          </button>
-          <h5 className="text-center">or</h5>
-          <Link type="button" className="btn btn-dark" to="">
-            {" "}
-            Create Account
-          </Link>
-          <br />
-          <h5>Follow us on</h5>
-          <span>
-          <BsFacebook color="#1773EA" size={30}/> &nbsp;
-          <AiFillTwitterCircle color="#1DA1F2 " size={30}/>
-          </span>
-          
+          <form onSubmit={formSubmit}>
+            <h1>Login </h1> <br />
+            <h5>Lets go gets started</h5>
+            <br />
+            <label>Email</label>
+            <input
+              className="btn1"
+              type="text"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              placeholder="Enter Your Username"
+            />
+            <br />
+            <br />
+            <h5>Password</h5>
+            <input
+              className="btn1"
+              name="password"
+              type="password"
+              onChange={event=>{setPassword(event.target.value)}}
+              placeholder="Enter Your Password"
+            />
+            <br />
+            <br />
+            <button type="submit"  className="btn btn-dark">
+              Submit
+            </button>
+            <h5 className="text-center">or</h5>
+            <Link type="button" className="btn btn-dark" to="">
+              {" "}
+              Create Account
+            </Link>
+            <br />
+            <h5>Follow us on</h5>
+            <span>
+              <BsFacebook color="#1773EA" size={30} /> &nbsp;
+              <AiFillTwitterCircle color="#1DA1F2 " size={30} />
+            </span>
+          </form>
         </div>
       </div>
     </div>
--- src/Components/Elements/Signin.jsx
+++ src/Components/Elements/Signin.jsx
@@ -1,32 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 import {Link } from 'react-scroll'
 import {BsFacebook} from 'react-icons/bs'
 import {AiFillTwitterCircle} from 'react-icons/ai'
 
 function Signin() {
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const formSubmit=(e)=>{
+      e.preventDefault()
+      console.log("name",name)
+      console.log("password",password)
+
+  }
   return (
     <div className="login1">
     <div className="container p-5  ">
       <div className=" card text-center  py-5  ">
+        <form onSubmit={formSubmit}>
         <h1>Sign up </h1> <br />
         <h5>Please Enter your Details to join us</h5>
         <br />
-        <h5>Email</h5>
+        <label>Name</label>
         <input
           className="btn1"
           type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
           placeholder="Enter Your Username"
         />
         <br />
-        <h5>Password</h5>
-        <input className="btn1" type="password" placeholder="Enter Your Password" />
+        <label>Password</label>
+        <input className="btn1" type="password"   onChange={event=>{setPassword(event.target.value)}} placeholder="Enter Your Password" />
         <br />
-        <Link type="button" className="btn btn-dark" to="">
+        <button type="submit" className="btn btn-dark" to="">
           {" "}
           Create Account
-        </Link>
+        </button>
         <br />
         <h5>Follow us on</h5>
+        </form>
         <span>
         <BsFacebook color="#1773EA" size={30}/> &nbsp;
         <AiFillTwitterCircle color="#1DA1F2 " size={30}/>
